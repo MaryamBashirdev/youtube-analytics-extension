c63aaf50aedf4cc32ea7422b00164cc7634e7787
Maryam Bashir <misbahfatima92@gmail.com>
2026-07-03 23:24:49 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Add your YouTube Data API v3 key here.
 // Restrict this key in Google Cloud Console (API restrictions + optional IP/referrer limits).
 const YT_CONFIG = {
-  API_KEY: "AIzaSyDHZRkdgd-qNJpAHTs2GUAccIteos82dAQ",
+  API_KEY: "USE YOUR API KEY",
 };
